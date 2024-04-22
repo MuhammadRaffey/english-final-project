42331b4a5f98864fb11c7488c6ae9c0f3bc0ff79
Muhammad Raffey <muhammadraffey26@gmail.com>
2024-04-22 19:22:24 +0500

    added icons

--- src/app/annas/page.tsx
+++ src/app/annas/page.tsx
@@ -11,35 +11,95 @@ const page = () => {
     <div className="flex gap-3 flex-col p-5">
       <nav className="navig">
         <Link
-          className="duration-75 delay-75 text-white  hover:underline hover:text-[#35fb91]  shadow-glow"
-          href={"/"}
+          href="/"
+          className="duration-75 delay-75 text-white hover:underline hover:text-[#35fb91] shadow-glow flex items-center"
         >
+          <svg
+            width="20px"
+            height="20px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#000000"
+            className="mr-2" // Adjust the margin as needed
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274"
+                stroke="#fcfcfc"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M15 18H9"
+                stroke="#fcfcfc"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+            </g>
+          </svg>
           Home
         </Link>
         <Link
           className="duration-75 delay-75 text-white  hover:underline hover:text-[#35fb91]  shadow-glow"
           href={"annas/huzaifa"}
         >
-          {" "}
+          <svg
+            width="20px"
+            height="20px"
+            fill="#ffffff"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            className="inline-block h-5 w-5 mr-2"
+          >
+            <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
+          </svg>
           Huzaifa
         </Link>
         <Link
           className="duration-75 delay-75 text-white  hover:underline hover:text-[#35fb91]  shadow-glow"
           href={"annas/akash"}
         >
-          {" "}
+          <svg
+            width="20px"
+            height="20px"
+            fill="#ffffff"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            className="inline-block h-5 w-5 mr-2"
+          >
+            <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
+          </svg>
           Akash
         </Link>
         <Link
           className="duration-75 delay-75 text-white  hover:underline hover:text-[#35fb91]  shadow-glow"
           href={"annas/rehan"}
         >
-          {" "}
+          <svg
+            width="20px"
+            height="20px"
+            fill="#ffffff"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            className="inline-block h-5 w-5 mr-2"
+          >
+            <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
+          </svg>
           Rehan
         </Link>
       </nav>
       <h1 className="font-bold text-center">Title: {pageData.title}</h1>
-      {/* Mapping over paragraphs to render them */}
+      {}
       <div className="text-center">
         {paragraphs.map((paragraph, index) => (
           <p
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,35 @@ const Home = () => {
     <div className="p-6  font-[Urbanist] overflow-hidden">
       <nav className="navig">
         <Link
-          href={"/annas"}
+          href="/annas"
           className="duration-75 delay-75 text-white hover:text-[#35fb91]  hover:underline shadow-glow"
         >
-          {" "}
+          <svg
+            width="20px"
+            height="20px"
+            fill="#ffffff"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            className="inline-block h-5 w-5 mr-2"
+          >
+            <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
+          </svg>
           Annas
         </Link>
         <Link
-          href={"/raffey"}
-          className="duration-75 delay-75 text-white  hover:underline hover:text-[#35fb91]  shadow-glow"
+          href="/raffey"
+          className="duration-75 delay-75 text-white hover:text-[#35fb91]  hover:underline shadow-glow"
         >
-          {" "}
+          <svg
+            fill="#ffffff"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            className="inline-block h-5 w-5 mr-2"
+          >
+            <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
+          </svg>
           Raffey
         </Link>
       </nav>
@@ -29,7 +47,7 @@ const Home = () => {
         </p>
       </div>
       <div className="flex items-center mb-[7rem] mt-14 justify-center">
-        <a href="#main-content">
+        <a href="#skip-to-main-content">
           <svg
             className="w-10 animate-bounce p-1 rounded-full ring-[3px] ring-[#3966ae] delay-75 duration-75 hover:ring-offset-4 h-10 text-[#fff]"
             fill="none"
@@ -43,7 +61,10 @@ const Home = () => {
           </svg>
         </a>
       </div>
-      <div id="main-content" className="flex items-center justify-center mb-10">
+      <div
+        id="skip-to-main-content"
+        className="flex items-center justify-center mb-10"
+      >
         <h1 className=" tracking-wider text-[#3966ae] hover:text-[#5f9dff] duration-75 cursor-help delay-75  font-semibold text-[35pt] sm:text-[50pt] text-center">
           What is Artificial Intelligence?
         </h1>
@@ -72,7 +93,7 @@ const Home = () => {
         </p>
       </div>
       <footer>
-        <h2 className="text-center leading-snug tracking-wider text-[50px] sm:text-[100px]   font-bold  cursor-pointer text-[#3966ae] mt-10">
+        <h2 className="text-center leading-snug tracking-wider text-[50px] sm:text-[100px]   font-bold  cursor-pointer text-[#3966ae] mt-5">
           Conclusion
         </h2>
         <p className="sm:text-center text-[20px] sm:text-2xls text-[#a8a8a8] delay-150 duration-150 hover:text-[#fff]">
