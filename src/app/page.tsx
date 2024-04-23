1e4155e2338fa3611466fa77ce1d626d551b74ba
Muhammad Raffey <muhammadraffey26@gmail.com>
2024-04-23 19:17:25 +0500

    some stuff added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
+import { mainPage } from "../../data/main";
+import Image from "next/image";
 
 const Home = () => {
+  const { main, foot } = mainPage;
+  const handleImageClick = (event: any) => {
+    event.target.classList.toggle("zoom");
+  };
+
   return (
     <div className="p-6  font-[Urbanist] overflow-hidden">
       <nav className="navig">
@@ -71,25 +79,7 @@ const Home = () => {
       </div>
       <div>
         <p className="sm:text-center text-[20px] sm:text-2xls text-white sm:text-[#a8a8a8] delay-150 duration-150 sm:hover:text-[#fff]">
-          Artificial Intelligence (AI) refers to the simulation of human
-          intelligence in machines that are programmed to mimic human cognitive
-          functions such as learning, problem-solving, perception, reasoning,
-          and decision-making. AI technologies enable computers and systems to
-          perform tasks that typically require human intelligence. These tasks
-          can range from simple ones like recognizing patterns in data to
-          complex activities like driving autonomous vehicles or playing
-          strategic games. AI encompasses various subfields, including machine
-          learning, natural language processing, computer vision, robotics,
-          expert systems, and more. Machine learning, in particular, has seen
-          significant advancements in recent years and is a subset of AI that
-          focuses on the development of algorithms and statistical models that
-          allow computers to perform tasks without explicit instructions but
-          instead rely on patterns and inference. Overall, AI aims to create
-          systems that can analyze and interpret complex data, adapt and learn
-          from experiences, and make decisions with little or no human
-          intervention. Its applications span across numerous industries,
-          including healthcare, finance, transportation, education,
-          entertainment, and beyond.
+          ${main}
         </p>
       </div>
       <footer>
@@ -97,14 +87,7 @@ const Home = () => {
           Conclusion
         </h2>
         <p className="sm:text-center text-[20px] sm:text-2xls text-white sm:text-[#a8a8a8] delay-150 duration-150 sm:hover:text-[#fff]">
-          Currently, in my view, AI resembles a small child in the process of
-          learning. One of the primary risks associated with AI decision-making
-          lies in its dependency solely on the data provided through machine
-          learning algorithms. These algorithms can both possess biases and lack
-          them, as they are reliant on the data they are trained on.
-          Consequently, if the data utilized to train an AI system contains
-          biases, the decisions made by the system will inevitably reflect those
-          biases.
+          ${foot}
         </p>
       </footer>
     </div>
